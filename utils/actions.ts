680c220ca4f02d688d111429cf8f2183f48fce7e
abdlla elshafay <abdllaelshaf3y@gmail.com>
2026-03-18 04:40:03 +0200

    first commit

---
+++ utils/actions.ts
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation";
+import { prisma } from "./db";
+export const fetchFeaturedProducts = async () => {
+  const products = await prisma.product.findMany({
+    where: {
+      featured: true,
+    },
+  });
+  return products;
+};
+
+export const fetchAllProducts = async ({ search = "" }: { search: string }) => {
+  return await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) redirect("/products");
+  return product;
+};
